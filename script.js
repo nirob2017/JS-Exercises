17bd2cacad0c3883ef2fa1319dff7a1fc2b97cd5
Md Monsur Hossain Nirob <nirob726@gmail.com>
2018-05-21 04:31:10 +0000

    task_2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 /* JavaScript code here */
 console.log("Internal JS");
+
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value, index){
+        sum += value;
+    });
+    return sum;
+}
+
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([1,20,-3,45]));
