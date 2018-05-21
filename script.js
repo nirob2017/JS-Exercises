f23885816f864f2aa2bf25e857e494491396d3ad
Md Monsur Hossain Nirob <nirob726@gmail.com>
2018-05-21 04:39:55 +0000

    task_3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-/* JavaScript code here */
+//Task 1
 console.log("Internal JS");
 
+//Task 2
 function sumArray(arr){
     var sum = 0;
     arr.forEach(function(value, index){
@@ -11,3 +12,13 @@ function sumArray(arr){
 
 console.log(sumArray([1,2,3,4,5]));
 console.log(sumArray([1,20,-3,45]));
+
+//Task 3
+function checkEmail(emailString){
+    var emailFormat = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    var match = emailFormat.test(emailString);
+    return match;
+    
+}
+console.log(checkEmail("John@smith.com"));
+console.log(checkEmail("Johnsmith.com"));
